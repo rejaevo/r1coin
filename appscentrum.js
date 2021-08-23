8034882cab3e9297b4476188df3b3276ef5c2a26
BuildTools <unconfigured@null.spigotmc.org>
2021-08-23 08:56:40 +0300

    Updates

--- appscentrum.js
+++ appscentrum.js
@@ -1,5 +1,5 @@
 
-import * as bridge from '/r1coin/vk-bridge';
+import * as bridge from 'https://rejaevo.github.io/r1coin/vk-bridge/';
 var clicks = 0; // change int to var here
 function exportw() {
 
