31749ebadc8dd5e7b0371e77e67c715896232bf7
BuildTools <unconfigured@null.spigotmc.org>
2021-08-23 08:17:24 +0300

    Updates

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import reportWebVitals from './reportWebVitals';
+import bridge from '@vkontakte/vk-bridge';
+var clicks = 0; // change int to var here
+function exportw() {
+
+    bridge.send("VKWebAppInit", {});
+        bridge.send("VKWebAppShowNativeAds", {ad_format:"reward"})
+.then(data => console.log(data.result))
+.catch(error => console.log(error));
+clicks += 40;
+        document.getElementById("clicks").innerHTML = clicks;
+
+}
